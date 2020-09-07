1f7d2accf10088334bd9fb12130a5f7954c3ff43
Sidharth <sidnagar08@gmail.com>
2020-09-07 16:40:11 +0530

    Profile

---
+++ app.js
@@ -0,0 +1,63 @@
+class particle{
+
+constructor(){
+	this.pos = createVector(random(width),random(height));
+	this.vel = createVector(random(-3,3),random(-3,3));
+	this.size = 8;
+	 }
+
+// ek single circle(particle) bnane k liye
+draw(){
+	noStroke();
+	fill('rgba(255,255,255,0.5)');
+	circle(this.pos.x, this.pos.y, this.size)
+	}
+
+// movement ko hum velocity add krke update kr rhe h
+update(){
+	this.pos.add(this.vel);
+	this.edges();
+	}
+
+// edges se bhar na jaye isliye edges detect krni padegi
+edges(){
+	if(this.pos.x < 0 || this.pos.x > width){
+		this.vel.x *= -1;
+	 }
+	if(this.pos.y < 0 || this.pos.y > height){
+		this.vel.y *= -1;
+	 }
+	}
+
+// particles ko ek line se connect krdenge agar zyada pass aa jate h toh
+line(){
+	particles.forEach((particle)=>{
+		const dis = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+		if(dis < 100){
+			stroke('rgba(255,255,255,0.1)');
+			line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+		}
+	});
+	}
+}
+
+// bhut sare particles k liye array bnali h
+const particles = [];
+
+function setup(){
+	createCanvas(window.innerWidth, window.innerHeight);
+
+	const noParticle = Math.floor(window.innerWidth/10);
+	for(let i=0; i<noParticle; i++){
+		particles.push(new particle);
+	}
+}
+
+function draw(){
+	background(251, 86, 86);
+	particles.forEach((p,index)=>{
+		p.update();
+		p.draw();
+		p.line(particles.slice(index));
+	});
+}
