6fb10fc53a1f174b243661e63e8c117b06ac7fe4
Sidharth <sidnagar08@gmail.com>
2020-09-11 22:12:46 +0530

    _blank

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 class particle{
 
-constructor(){
-	this.pos = createVector(random(width),random(height));
-	this.vel = createVector(random(-3,3),random(-3,3));
-	this.size = 8;
-	 }
-
-// ek single circle(particle) bnane k liye
-draw(){
-	noStroke();
-	fill('rgba(255,255,255,0.5)');
-	circle(this.pos.x, this.pos.y, this.size)
+	constructor(){
+		this.pos = createVector(random(width),random(height));
+		this.vel = createVector(random(-2,2),random(-2,2));
+		this.size = 8;
 	}
 
-// movement ko hum velocity add krke update kr rhe h
-update(){
-	this.pos.add(this.vel);
-	this.edges();
+	// ek single circle(particle) bnane k liye
+	draw(){
+		noStroke();
+		fill('rgba(255,255,255,0.5)');
+		circle(this.pos.x, this.pos.y, this.size)
 	}
 
-// edges se bhar na jaye isliye edges detect krni padegi
-edges(){
-	if(this.pos.x < 0 || this.pos.x > width){
-		this.vel.x *= -1;
-	 }
-	if(this.pos.y < 0 || this.pos.y > height){
-		this.vel.y *= -1;
-	 }
+	// movement ko hum velocity add krke update kr rhe h
+	update(){
+		this.pos.add(this.vel);
+		this.edges();
 	}
 
-// particles ko ek line se connect krdenge agar zyada pass aa jate h toh
-line(){
-	particles.forEach((particle)=>{
-		const dis = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
-		if(dis < 100){
-			stroke('rgba(255,255,255,0.1)');
-			line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+	// edges se bhar na jaye isliye edges detect krni padegi
+	edges(){
+		if(this.pos.x < 0 || this.pos.x > width){
+			this.vel.x *= -1;
 		}
-	});
+		if(this.pos.y < 0 || this.pos.y > height){
+			this.vel.y *= -1;
+		}
+	}
+
+	// particles ko ek line se connect krdenge agar zyada pass aa jate h toh
+	line(){
+		particles.forEach((particle)=>{
+			const dis = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+			if(dis < 100){
+				stroke('rgba(255,255,255,0.1)');
+				line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+			}
+		});
 	}
 }
 
@@ -46,7 +46,6 @@ const particles = [];
 
 function setup(){
 	createCanvas(window.innerWidth, window.innerHeight);
-
 	const noParticle = Math.floor(window.innerWidth/10);
 	for(let i=0; i<noParticle; i++){
 		particles.push(new particle);
